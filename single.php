d9ec82b53de0e28c58b5da45aa046d7e1263652e
MarkW <mark@liftedlogic.com>
2018-05-21 12:59:23 -0500

    Single Post page in place

--- single.php
+++ single.php
@@ -1,10 +1,12 @@
+<?php include( locate_template('templates/contents/content-hero.php') ); ?>
 <article <?php post_class('content'); ?>>
   <div class="container row">
 <?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/contents/content', 'components'); ?>
+  <?php get_template_part('templates/contents/content', get_post_type()); ?>
 <?php endwhile; ?>
   </div>
 </article>
+<?php get_template_part('templates/contents/content', 'callout'); ?>
 <?php
   $links = get_field('relationships');
 
