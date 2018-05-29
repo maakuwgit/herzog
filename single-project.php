5fca505ee85765db2bce23407bc8c602f08dd4a3
MarkW <mark@liftedlogic.com>
2018-05-29 12:20:02 -0500

    WIP functional page. Dropping in the hero content and globe

--- single-project.php
+++ single-project.php
@@ -1,3 +1,17 @@
+<?php
+  $location = 'Kansas City, MO';
+  while (have_posts()) : the_post();
+?>
+<picture class="globe col" id="<?php echo 'project-'.get_the_ID().'-globe'; ?>" data-globe>
+  <!-- Dev Note: put the texture image in here for SEO -->
+</picture>
+<main class="content hero">
+  <div class="wrapper row start">
+    <h1 class="col"><?php the_title(); ?></h1>
+    <h2 class="text-light col"><?php echo $location; ?></h2>
+  </div>
+</main>
+<?php endwhile; ?>
 <article <?php post_class('content'); ?>>
   <div class="container row">
 <?php while (have_posts()) : the_post(); ?>
